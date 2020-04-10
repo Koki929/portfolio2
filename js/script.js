1a6892ce8ad1094921eb088b8fc978e70b92a5e3
Koki929 <2013edcckokimatsushita@gmail.com>
2020-04-10 14:21:20 +0900

    編集4/10

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ jQuery(window).on('scroll', function ($) {
 		jQuery('.floating').hide();
 	}
 });
+
+
+/* scroll */
+$(function(){
+	$('a[href^="#"]').click(function() {
+		var speed = 400;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position}, speed, 'swing');
+		return false;
+	});
+});
