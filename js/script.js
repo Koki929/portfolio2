fb1fb13740d835598a1bc460ea1e73fd2a6543b1
Koki929 <2013edcckokimatsushita@gmail.com>
2020-04-10 17:41:19 +0900

    編集2 4/10

--- js/script.js
+++ js/script.js
@@ -29,3 +29,22 @@ $(function(){
 		return false;
 	});
 });
+
+/* movetext*/
+$('.movetext').each(function(){
+    //一文字ずつ<span>で括る
+    $(this).children().addBack().contents().each(function() {
+        if (this.nodeType == 3) {
+        $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+        }
+    });
+     
+　　//inviewを使って画面に表れたら起動させる
+    $(this).on('inview',function(){
+        //一文字ずつ順番に不透明させる
+        $(this).css({'opacity':1});
+        for (var i = 0; i <= $(this).children('span').length; i++) {
+        $(this).children('span').eq(i).delay(40*i).animate({'opacity':1},800);
+        };
+    });
+});
